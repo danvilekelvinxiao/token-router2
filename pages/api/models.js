6823376e79741d4c0d57aeb2608d19a2937ec204
DanvileKelvinXiao <danvilekelvinxiao@DanvileKelvinXiaodeMacBook-Air.local>
2026-05-27 12:17:32 +0800

    feat: 完成内容管理持久化存储

--- pages/api/models.js
+++ pages/api/models.js
@@ -1,19 +1,7 @@
-import { MODEL_CATALOG, USD_TO_CNY_RATE } from "@/lib/models";
+import { getPublicModels } from "@/lib/content-store";
 
 export default function handler(req, res) {
-  const models = [...MODEL_CATALOG].sort((a, b) => {
-    const aTotal = a.inputPrice + a.outputPrice;
-    const bTotal = b.inputPrice + b.outputPrice;
-    return aTotal - bTotal;
-  }).map((model) => {
-    return {
-      ...model,
-      inputPriceCny: Number((model.inputPrice * USD_TO_CNY_RATE).toFixed(4)),
-      outputPriceCny: Number((model.outputPrice * USD_TO_CNY_RATE).toFixed(4)),
-      currency: "CNY",
-      unit: "per_1m_tokens",
-    };
-  });
+  const models = getPublicModels();
 
-  return res.status(200).json({ models });
+  return res.status(200).json({ source: "content", models });
 }
